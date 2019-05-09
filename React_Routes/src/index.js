c57524ccdf9b3a284b74b48f461919b230ebf6b2
vanessa diminich <vanessadiminich@gmail.com>
2019-05-09 19:03:54 -0400

    reacthw

--- React_Routes/src/index.js
+++ React_Routes/src/index.js
@@ -1,18 +1,38 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import './CSS/index.css';
-
-import Happy from './components/Happy';
-import Sleepy from './components/Sleepy';
-import Guilty from './components/Guilty';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./CSS/index.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Happy from "./components/Happy";
+import Sleepy from "./components/Sleepy";
+import Guilty from "./components/Guilty";
 
 // your code goes here
-class Home extends Component{
-  render(){
-    return(
-      // your code goes here
-    )
+class Home extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/happy">Happy</Link>
+            </li>
+            <li>
+              <Link to="/sleepy">Sleepy</Link>
+            </li>
+            <li>
+              <Link to="/guilty">Guilty</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Route exact path="/happy" component={Happy} />
+          <Route path="/sleepy" component={Sleepy} />
+          <Route path="/guilty" component={Guilty} />
+        </div>
+      </Router>
+    );
   }
 }
 
-ReactDOM.render( <Home />, document.getElementById('root') );
+ReactDOM.render(<Home />, document.getElementById("root"));
